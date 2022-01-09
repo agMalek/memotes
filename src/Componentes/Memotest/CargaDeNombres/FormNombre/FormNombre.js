feca549098860475b6f2554e8103e5ee36c129fc
agMalek <albermaleh.99@gmail.com>
2022-01-09 18:55:20 -0300

    mapeo los circulos de colores

--- src/Componentes/Memotest/CargaDeNombres/FormNombre/FormNombre.js
+++ src/Componentes/Memotest/CargaDeNombres/FormNombre/FormNombre.js
@@ -5,20 +5,7 @@ import { useEffect, useState } from 'react';
 const FormNombre = ({setNombre, setColor, jugadores, i, setCantJugadores, limpiarValores}) => {
 
     const [activo, setActivo] = useState(i >=2 ? false : true)
-    /* const [colores, setColores] = useState(
-        {
-            red: false,
-            orange: false,
-            yellow: false,
-            greenyellow: false,
-            green: false,
-            blue: false,
-            lightblue: false,
-            pink: false,
-            purple: false,
-            black: false
-        }
-    ) */
+    const colores = ["red", "orange", "yellow", "greenyellow", "green", "blue", "lightblue", "pink", "purple", "black"]
 
     const validarColor = (color) =>{
         let rta = true
@@ -60,39 +47,33 @@ const FormNombre = ({setNombre, setColor, jugadores, i, setCantJugadores, limpia
     return ( 
         <div className='contenedorFormNombre' style={{opacity: activo ? "1" : ".5"}}>
             <div className='d-flex'>
-                {i >= 2 ? <Switch onClick={() => onSwitch()} /> : ""}
+                {i >= 2 ? <Switch onClick={() => onSwitch()} /> : "" }
                 <h3 className='text-center mb-3 nombre' style={{width: i >= 2 ? "80%" :"100%", color: `${jugadores[`jugador${i+1}`].color}` === "" ? "white" : `${jugadores[`jugador${i+1}`].color}`}} >
                     {jugadores[`jugador${i+1}`].nombre === "" ? `Jugador ${i+1}` : jugadores[`jugador${i+1}`].nombre}
                 </h3>
             </div>
             <input
-                className='w-100 mb-3' 
+                className='w-100 mb-3 inputNombre' 
                 name={`jugador${i+1}`} 
                 type="text" 
                 value={jugadores[`jugador${i+1}`].nombre} 
                 onChange={(e) => setNombre(e)}
                 readOnly={!activo}
+                placeholder='Ingrese el nombre'
             />
-            <div className='d-flex'>
-                <div className='itemBarraColor itemBarraColor-red' style={{opacity: jugadores[`jugador${i+1}`].color === "red" || validarColor("red") ? '1' : "0.3"}} onClick={activo && validarColor("red") ? () => onColor("red", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-orange' style={{opacity: jugadores[`jugador${i+1}`].color === "orange" || validarColor("orange") ? '1' : "0.3"}} onClick={activo && validarColor("orange") ? () => onColor("orange", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-yellow' style={{opacity: jugadores[`jugador${i+1}`].color === "yellow" || validarColor("yellow") ? '1' : "0.3"}} onClick={activo && validarColor("yellow") ? () => onColor("yellow", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-greenyellow' style={{opacity: jugadores[`jugador${i+1}`].color === "greenyellow" || validarColor("greenyellow") ? '1' : "0.3"}} onClick={activo && validarColor("greenyellow") ? () => onColor("greenyellow", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-green' style={{opacity: jugadores[`jugador${i+1}`].color === "green" || validarColor("green") ? '1' : "0.3"}} onClick={activo && validarColor("green") ? () => onColor("green", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-blue' style={{opacity: jugadores[`jugador${i+1}`].color === "blue" || validarColor("blue") ? '1' : "0.3"}} onClick={activo && validarColor("blue") ? () => onColor("blue", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-lightblue' style={{opacity: jugadores[`jugador${i+1}`].color === "lightblue" || validarColor("lightblue") ? '1' : "0.3"}} onClick={activo && validarColor("lightblue") ? () => onColor("lightblue", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-pink' style={{opacity: jugadores[`jugador${i+1}`].color === "pink" || validarColor("pink") ? '1' : "0.3"}} onClick={activo && validarColor("pink") ? () => onColor("pink", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-purple' style={{opacity: jugadores[`jugador${i+1}`].color === "purple" || validarColor("purple") ? '1' : "0.3"}} onClick={activo && validarColor("purple") ? () => onColor("purple", i) : () => console.log("Bloquedadp")}></div>
-                <div className='itemBarraColor itemBarraColor-black' style={{opacity: jugadores[`jugador${i+1}`].color === "black" || validarColor("black") ? '1' : "0.3"}} onClick={activo && validarColor("black") ? () => onColor("black", i) : () => console.log("Bloquedadp")}></div>
-                {/* <div className='itemBarraColor itemBarraColor-orange' onClick={() => onColor("orange", i)}></div>
-                <div className='itemBarraColor itemBarraColor-yellow' onClick={() => onColor("yellow", i)}></div>
-                <div className='itemBarraColor itemBarraColor-greenyellow' onClick={() => onColor("greenyellow", i)}></div>
-                <div className='itemBarraColor itemBarraColor-green' onClick={() => onColor("green", i)}></div>
-                <div className='itemBarraColor itemBarraColor-blue' onClick={() => onColor("blue", i)}></div>
-                <div className='itemBarraColor itemBarraColor-lightblue' onClick={() => onColor("lightblue", i)}></div>
-                <div className='itemBarraColor itemBarraColor-pink' onClick={() => onColor("pink", i)}></div>
-                <div className='itemBarraColor itemBarraColor-purple' onClick={() => onColor("purple", i)}></div>
-                <div className='itemBarraColor itemBarraColor-black' onClick={() => onColor("black", i)}></div> */}
+            <p className='text-white text-center my-1'>Elija un color que lo represente</p>
+            <div className='d-flex justify-content-evenly'>
+                {
+                    colores.map(col => (
+                        <div 
+                            className={`itemBarraColor itemBarraColor-${col}`} 
+                            style={{opacity: jugadores[`jugador${i+1}`].color === col || validarColor(col) ? '1' : "0.3"}} 
+                            onClick={activo && validarColor(col) ? () => onColor(col, i) : () => console.log("Bloqueado")}
+                        >
+                            {jugadores[`jugador${i+1}`].color === col ? "OK" : ""}
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
