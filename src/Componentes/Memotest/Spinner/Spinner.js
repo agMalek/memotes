e251565dd67dd0abb69e89f33b93885bdd85c2ed
agMalek <albermaleh.99@gmail.com>
2022-01-11 21:08:27 -0300

    p de cargando.. en spinner basico

--- src/Componentes/Memotest/Spinner/Spinner.js
+++ src/Componentes/Memotest/Spinner/Spinner.js
@@ -2,9 +2,11 @@ import './Spinner.css'
 
 const Spinner = () => {
     return ( 
-    
-        <div className="spinner-border text-light" role="status">
-            <span className="visually-hidden">Loading...</span>
+        <div className='d-flex flex-column align-items-center'>
+            <div className="spinner-border text-light" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+            <h4 className='text-white my-4'>Cargando...</h4>
         </div>
  
     );
